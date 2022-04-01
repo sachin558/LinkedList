461612923ce1489e30cd95acaf0c687a30d6c1de
Sachin <sachin.kumar_cs20@gla.ac.in>
2022-04-01 23:01:16 +0530

    BinarTreeFac

---
+++ src/BinarTreeFac.java
@@ -0,0 +1,31 @@
+public class BinarTreeFac {
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public Node root;
+    public BinarTreeFac(){
+        root=null;
+    }
+    public int factorial(int num){
+        int fact=1;
+        if (num==0){
+            return 1;
+        }else {
+            while (num>1){
+                fact=fact*1;
+                num--;
+            }
+            return fact;
+        }
+    }
+
+    public static void main(String[] args) {
+        BinarTreeFac obj=new BinarTreeFac();
+        System.out.println(obj.factorial(5));
+    }
+}
