d2d0db41fd309d42f8b6ca05e993023e82a82d15
Sachin <sachin.kumar_cs20@gla.ac.in>
2022-04-02 22:36:28 +0530

    ListNode

---
+++ src/cl.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class cl {
+    public static void main(String[] args) {
+      String str=new String("7");
+        System.out.println(1+102+str+1+110);
+    }
+}
