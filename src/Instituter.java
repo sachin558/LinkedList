21d2dccf55f38d4a27c32a6c6ab2cb9f6e9c2352
Sachin <sachin.kumar_cs20@gla.ac.in>
2022-04-03 13:11:30 +0530

    Instituter

---
+++ src/Instituter.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class  Instituter {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner( System.in);
+        System.out.println("enter the phone no");
+        int a=sc.nextInt();
+        System.out.println("enter name ofe the user ");
+        String b = sc.next();
+
+        System.out.println("the phone no entred by user "+ a);
+
+
+        System.out.println("the name of the customer "+b);
+    }
+}
