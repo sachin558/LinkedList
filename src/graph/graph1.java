6e1235d991bb6990a0f3cbc143664be17c0ede59
Sachin <sachin.kumar_cs20@gla.ac.in>
2022-04-18 16:37:34 +0530

    Graph1

--- src/graph/graph1.java
+++ src/graph/graph1.java
@@ -9,11 +9,17 @@ public class graph1 {
     public static void main(String[] args) {
         HashMap<Integer,Integer> obj=new HashMap<>();
         obj.put(10,100);
+        obj.put(20,200);
+        obj.put(30 ,400);
 //        obj.clear();
 //        obj.clone();
-        obj.containsKey(10);
-        System.out.println(obj.get(10));
-        System.out.println(obj.getOrDefault(2,0));
+//        obj.containsKey(10);
+//        System.out.println(obj.get(10));
+//        System.out.println(obj.getOrDefault(2,0));
+        for (Map.Entry<Integer,Integer> res: obj.entrySet()){
+            System.out.print(res.getKey() + " ");
+            System.out.println(res.getValue()+" ");
+        }
     }
 
 }
